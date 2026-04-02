57bb0b1f3f4d9d8089df9c45029c7f664163fa0e
arpit jana <arpitjana2103@gmail.com>
2026-04-02 17:45:44 +0530

    (be) doc(logger.config): add doc for logger

--- backend/src/config/logger.config.ts
+++ backend/src/config/logger.config.ts
@@ -13,3 +13,24 @@ export const logger = pino({
         },
     },
 });
+
+/*
+| Level   | Value | Use case                 |
+| ------- | ----- | ------------------------ |
+| `fatal` | 60    | App crash, unrecoverable |
+| `error` | 50    | Failed operation         |
+| `warn`  | 40    | Unexpected but handled   |
+| `info`  | 30    | Normal events            |
+| `debug` | 20    | Dev debugging            |
+| `trace` | 10    | Very detailed, noisy     |
+
+fatal > error > warn > info > debug > trace
+Higher number = more severe
+
+logger.fatal("System crash");
+logger.error({ err }, "DB failed");
+logger.warn("Cache miss");
+logger.info("Server started");
+logger.debug("User payload", { user });
+logger.trace("Function entry");
+*/
