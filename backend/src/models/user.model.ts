a5535c6289a669f3d188f5c9db229096bbfc2eb6
arpit jana <arpitjana2103@gmail.com>
2026-04-06 17:21:07 +0530

    (be) refactor(user.model): refactor user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,6 +1,6 @@
-import type { Document, Types } from "mongoose";
+import type { HydratedDocument, Model, QueryWithHelpers, Types } from "mongoose";
 
-import mongoose, { Schema as MongooseSchema } from "mongoose";
+import mongoose from "mongoose";
 
 import { ModelEnum } from "../enums/model.enum.js";
 import { bcryptCompare, bcryptHash } from "../utils/bcrypt.util.js";
@@ -18,12 +18,20 @@ type UserProps = {
     emailVerified: boolean;
 };
 
-export interface UserDocument extends Document, UserProps {
-    comparePassword(value: string): Promise<boolean>;
+type UserMethods = {
+    comparePassword(candidate: string): Promise<boolean>;
     omitPassword(): Omit<UserProps, "password">;
-}
+};
 
-const userSchema = new MongooseSchema<UserDocument>(
+type UserQueryHelper = {
+    byEmail(email: string): UserQuery<UserDoc | null>;
+};
+
+export type UserDoc = HydratedDocument<UserProps, UserMethods>;
+type UserQuery<R> = QueryWithHelpers<R, UserDoc, UserQueryHelper>;
+type UserModel = Model<UserProps, UserQueryHelper, UserMethods>;
+
+const userSchema = new mongoose.Schema<UserProps, UserModel, UserMethods, UserQueryHelper>(
     {
         name: {
             type: String,
@@ -62,6 +70,11 @@ const userSchema = new MongooseSchema<UserDocument>(
                 return bcryptCompare(value, this.password);
             },
         },
+        query: {
+            byEmail(this: UserQuery<UserDoc | null>, email: string) {
+                return this.findOne({ email });
+            },
+        },
     },
 );
 
@@ -73,5 +86,5 @@ userSchema.pre("save", async function () {
     }
 });
 
-const UserModel = mongoose.model<UserDocument>(ModelEnum.USER, userSchema);
+const UserModel = mongoose.model<UserProps, UserModel>(ModelEnum.USER, userSchema);
 export default UserModel;
