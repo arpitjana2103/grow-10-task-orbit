59a1dc27f4fb49a6a432e152fdead07ba243b60d
arpit jana <arpitjana2103@gmail.com>
2026-04-19 15:36:53 +0530

    (fe) style: format whole codebase

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import path from "path";
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -6,10 +7,10 @@ import oxlint from "vite-plugin-oxlint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), oxlint()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss(), oxlint()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
